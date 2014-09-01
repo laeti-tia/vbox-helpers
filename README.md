48da0aa79836278e78c04f23e8d147c3d4d6870a
Antoine Delvaux <antoine@eshango.com>
2014-09-01 18:34:58 +0000

    Adding a first version of the vbox-zvol-snapshot.sh script. First documentation draft.

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 vbox.helpers
 ============
 
-Collection of scripts to help with managing VirtualBox guests through the CLI
+A collection of bash scripts to help with managing VirtualBox guests through the UNIX CLI.
+
+
+Usage
+-----
+Most of these scripts assume you have VirtualBox and zfs installed and in your path.  ZFS is used to create ZVOL that act as raw devices to be used as VM disks.
+
+See documentation within each script for more information.
+
+
+Prerequisite
+------------
+For the ZVOL commands to work for the VM owner, this user (or the `vboxusers` group in FreeBSD, for example) need to have access to some zfs management commands.  Usually you can allow that with the following command (provided `tank/vm/disks` is the root of the ZVOL hierarchy):
+
+    zfs allow -g vboxusers clone,snapshot,destroy,rename,rollback tank/vm/disks
+
+
+Compatibility
+-------------
+The scripts have been tested under FreeBSD 9.3 running VirtualBox 4.3.12.  They need a working bash installation.
+
+
+Copyright and License
+---------------------
+© 2014 — Antoine Delvaux — All rights reserved.
+
+See enclosed LICENSE file.
---
+++ vbox-zvol-snapshot.sh
@@ -0,0 +1,55 @@
+#!/usr/local/bin/bash
+# This script creates a new ZFS ZVOL snapshot for an existing VM
+# It needs to be run as the user owning the VM.
+# The following permissions also need to be set on the ZVOL (or its hierarchy):
+#      clone,create,mount,snapshot,destroy,rename,rollback
+
+### Constants
+# The zfs mount point where are stored the ZVOLs
+zvol_path='tank/vm/disks/'
+curr='Clean'
+prev='Previous'
+
+### User supplied variables
+# The full VM path, it can take subdirectories under $zvol_path
+VM_path=$1
+VM=${VM_path/*\//}
+
+echo -e "We're going to rotate the \033[38;5;12m@$curr\033[0m snapshot of \033[38;5;12m$VM\033[0m."
+# Check VM exists, has ZVOL disks and is not running
+VBoxManage showvminfo $VM > /dev/null
+if [ $? -ne 0 ]; then
+    echo -e "\033[31mThe VM \033[38;5;12m$VM\033[0;31m doesn't seem to be existing.\033[0m"
+    exit
+fi
+zfs list $zvol_path$VM_path > /dev/null
+if [ $? -ne 0 ]; then
+    echo -e "\033[31mThe VM \033[38;5;12m$VM\033[0;31m doesn't seem to have ZVOL disks at \033[38;5;12m$VM_path\033[0m"
+    exit
+fi
+VBoxManage list runningvms | grep $VM > /dev/null
+if [ $? -eq 0 ]; then
+    echo -e "\033[31mThe VM \033[38;5;12m$VM\033[0;31m seems to be running, it is not recommended I take a ZVOL snapshot now.\033[0m"
+    exit
+fi
+
+# Rotate existing snapshot and take a new one
+# TODO: add the possibilty to use other snapshot names
+zfs destroy $zvol_path$VM_path@Previous
+if [ $? -ne 0 ]; then
+    echo -e "\033[38;5;214mSomething went wrong trying to destroy \033[38;5;12m$zvol_path$VM_path@Previous\033[0m"
+    echo "I continue anyway, but something might go wrong later."
+fi
+zfs rename $zvol_path$VM_path@Clean $zvol_path$VM_path@Previous
+if [ $? -ne 0 ]; then
+    echo -e "\033[31mSomething went wrong trying to rename \033[38;5;12m$zvol_path$VM_pathi@Clean to $zvol_path$VM_pathi@Previous\033[0m"
+    echo "I quit!"
+    exit
+fi
+zfs snapshot $zvol_path$VM_path@Clean
+if [ $? -ne 0 ]; then
+    echo -e "\033[31mSomething went wrong trying to create \033[38;5;12m$zvol_path$VM_path@Clean\033[0m"
+else
+    echo -e "We took a new snapshot of \033[1m$VM\033[0m at \033[38;5;12m$zvol_path$VM_path@Clean\033[0m and rotated the previous one at \033[38;5;12m$zvol_path$VM_path@Previous\033[0m"
+fi
+
