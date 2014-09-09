4aa460c4b67ebf9aeca48a0d5c1e76aff0dc9895
Antoine Delvaux <antoine@eshango.com>
2014-09-09 13:51:42 +0000

    Completing script to clone a VM and its ZVOL disk.

--- vbox-vm-clone.sh
+++ vbox-vm-clone.sh
@@ -7,13 +7,19 @@
 #  - the VM name, with an optional path prefix
 
 ### Constants
-# The zfs mount point where are stored the ZVOLs
-zvol_path='tank/vm/disks/'
+zvol_root='/dev/zvol/'
+# The zfs pool where are stored the ZVOLs
+zfs_pool='tank/vm/disks/'
+# ZFS snapshot names for a clean state and the previous clean state
 curr='Clean'
 prev='Previous'
+# VirtualBox VM root
+vbox_root='/tank/vm/'
+# VM snapshot to be cloned
+tbc='ToBeCloned'
 
 ### User supplied variables
-# $1: the full VM path, it can take subdirectories under $zvol_path
+# $1: the full VM path, it can take subdirectories under $zfs_pool
 if [ -z $1 ]; then
     echo -e "\033[31mPlease provide a VM name/path to make a snapshot.\033[0m"
     exit 1
@@ -21,22 +27,25 @@ fi
 VM_path=$1
 VM=${VM_path/*\//}
 
-# $2: the full path for the new VM, it can take subdirectories under $zvol_path
+# $2: the full path for the new VM, it can take subdirectories under $zfs_pool
 if [ -z $2 ]; then
     echo -e "\033[31mPlease provide a VM name/path for the new VM.\033[0m"
     exit 1
 fi
 new_VM_path=$2
 new_VM=${new_VM_path/*\//}
+new_VM_group='/'${new_VM_path%%\/*}
 
 echo -e "We're going to clone \033[38;5;12m$VM_path\033[0m@\033[38;5;12m$curr\033[0m into \033[38;5;12m$new_VM_path\033[0m@\033[38;5;12m$curr\033[0m to make a new VM named \033[38;5;12m$new_VM\033[0m."
+
+### Checks
 # Check VM exists, has ZVOL disks and the $curr snapshot exists
 VBoxManage showvminfo $VM > /dev/null
 if [ $? -ne 0 ]; then
     echo -e "\033[31mThe VM \033[38;5;12m$VM\033[0;31m doesn't seem to be existing.\033[0m"
     exit 1
 fi
-zfs list $zvol_path$VM_path@$curr > /dev/null
+zfs list $zfs_pool$VM_path@$curr > /dev/null
 if [ $? -ne 0 ]; then
     echo -e "\033[31mThe VM \033[38;5;12m$VM\033[0;31m doesn't seem to have a snapshot of ZVOL disks at \033[38;5;12m$VM_path@$curr\033[0m"
     exit 1
@@ -54,9 +63,27 @@ if [ $ret -ne 1 ]; then
     echo "I continue anyway, but something might go wrong later."
 fi
 
-# First we clone the ZVOL
-# Second we create a new VMDK
-# Third we create a new VM
-# Forth we attach the VMDK to the newly created VM
+### Action
+# 1. We clone the ZVOL and take a initial snapshot
+zfs clone ${zfs_pool}${VM_path}@${curr} ${zfs_pool}${new_VM_path}
+if [ $? -ne 0 ]; then
+    echo -e "\033[31mThere was a problem when cloning \033[38;5;12m$VM_path@$curr\033[31m to \033[38;5;12m$new_VM_path@$curr\033[0m"
+    exit 1
+fi
+zfs snapshot ${zfs_pool}${new_VM_path}@${curr}
 
-# All done!
+# 2. We clone the VM and remove any associated disk
+VBoxManage clonevm $VM --snapshot $tbc --mode machine --options link --name $new_VM --groups $new_VM_group --register
+vmdk_snap=`VBoxManage list hdds | awk "/\/${new_VM}\/Snapshots\// {print \\$2}"`
+echo $vmdk_snap
+VBoxManage storageattach $new_VM --storagectl SATA --port 0 --medium none
+VBoxManage closemedium disk "$vmdk_snap" --delete
+
+# 3. We create a new VMDK
+VBoxManage internalcommands createrawvmdk -filename ${vbox_root}${new_VM_path}/${new_VM}.vmdk -rawdisk ${zvol_root}${zfs_pool}${new_VM_path}
+
+# 4. We attach the VMDK to the newly created VM
+VBoxManage storageattach $new_VM --storagectl SATA --port 0 --type hdd --medium ${vbox_root}${new_VM_path}/${new_VM}.vmdk
+
+echo -e "\033[38;5;12m${new_VM}\033[0m was created with its disk as \033[38;5;12m${zvol_root}${zfs_pool}${new_VM_path}\033[0m"
+
